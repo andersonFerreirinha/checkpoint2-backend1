4942dd4228cbec05c502f29cb07b7de7401c6e8a
Anderson <anderson_ferreirinha@hotmail.com>
2022-09-05 17:26:26 -0300

    função paciente

---
+++ Dentista/Dentista/src/main/java/com/dh/grupo05/Dentista/service/PacienteService.java
@@ -0,0 +1,35 @@
+package com.dh.grupo05.Dentista.service;
+
+import com.dh.grupo05.Dentista.dao.IDao;
+import com.dh.grupo05.Dentista.model.Endereco;
+import com.dh.grupo05.Dentista.model.Paciente;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PacienteService {
+
+    @Autowired
+    IDao<Paciente> pacienteDAOH2;
+
+    public Paciente salvar(Paciente paciente) throws SQLException {
+        return pacienteDAOH2.salvar(paciente);
+    }
+
+    public List<Paciente> buscarTodosPacientes() throws SQLException{
+        return pacienteDAOH2.consultar();
+    }
+
+    public Optional<Paciente> buscaPorId(int id) throws SQLException{
+        return pacienteDAOH2.buscaPorId(id);
+    }
+
+    public void modificar(Paciente paciente) throws SQLException {
+        pacienteDAOH2.modificar(paciente);
+    }
+    
+}
